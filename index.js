648933d67bcaa10c5647d2e98fa0515f920677d0
“[firstname <faith.gacheri@student.moringaschool.com>
2022-07-17 15:31:26 +0300

    add function monthlyIncome to js

--- index.js
+++ index.js
@@ -20,15 +20,29 @@ function overTime () {
 
     var weekly = output * 7 * 45;
     var yearly = output * 365 * 45;
+    console.log(weekly)
  
     for ( let i = 0; i < litres.length; i ++){
         let parentUl = document.getElementById("litres")
         let list = document.createElement("li");
-        list.innerHTML = "your weekly income will be" + " " + "output" + " " + "weekly";
-        list.innerHTML = "your yearly income will be" + " " + "output" + " " + "monthly";
+        list.innerHTML = "your weekly income will be" + " " + "output" ;
+        list.innerHTML = "your yearly income will be" + " " + "output" ;
         parentUl.appendChild(list);
 }
 
 
 
+}
+
+function monthlyIncome () {
+    months =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    income =[2617020, 2448180, 2617020, 2532600, 2617020, 2532600, 2617020, 2617020, 2532600, 2617020, 2532600, 2617020
+    ]
+
+    for ( let i = 0; i < months.length; i++){
+        let parentUl = document.getElementById("monthly")
+        let list = document.createElement("li");
+        list.innerHTML = "your income for month " + " " + months[i] + " " + "is" + " " + income[i]
+        parentUl.appendChild(list);
+} 
 }
