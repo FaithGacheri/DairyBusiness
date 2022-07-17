734b0b077f4b64886b463c1dd3ff979177310736
“[firstname <faith.gacheri@student.moringaschool.com>
2022-07-17 12:09:43 +0300

    add for loop in js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var output = []
+
 function shedOutput (){
     sheds =[1, 2, 3, 4]
     litres =[510, 308, 486, 572]
+    sheds array.forEach(element => {
+        console.log(element)
+    });
+
+    for ( let i = 0; i < sheds.length; i++){
+        output += sheds[i] + "<br>"; litres[i]
+        console.log(output)
+    }
 }
