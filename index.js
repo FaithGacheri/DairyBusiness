a9d3d69f8f5170894804bf976a16e79ac4cffa95
“[firstname <faith.gacheri@student.moringaschool.com>
2022-07-17 14:44:50 +0300

    edit id sheds

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-var output = []
+var output = 0;
 
-function shedOutput (){
+function shedOutput (fn){
     sheds =[1, 2, 3, 4]
     litres =[510, 308, 486, 572]
-    sheds array.forEach(element => {
-        console.log(element)
-    });
-
+    
     for ( let i = 0; i < sheds.length; i++){
-      let parentUI = document.
+        let parentUl = document.getElementById("sheds")
+        let list = document.createElement("li");
+        list.innerHTML = "your production in shed number" + " " + sheds[i] + " " + "is" + " " +
+        litres[i] + " " + "per day";
+        parentUl.appendChild(list);
+        
+        output += litres[i] 
     }
+    return(output);
+}
+
+function overTime () {
+
+    var weekly = output * 7;
+    var yearly = output * 365;
+ 
+    for ( let i = 0; i < litres.length; i++){
+        let parentUl = document.getElementById("litres")
+        let list = document.createElement("li");
+        list.innerHTML = "your weekly income will be" + " " + "output" + " " + "weekly";
+        list.innerHTML = "your monthly income will be" + " " + "output" + " " + "monthly";
+        parentUl.appendChild(list);
+}
+
+
+
 }
